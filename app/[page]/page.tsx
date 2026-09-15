057bb32b30b658931cfc23c523fddc29f49ba4e4
mutsumilabjp-eng <mutsumi.lab.jp@gmail.com>
2026-09-15 15:59:56 +0900

    fix: satisfy lint for static-page internal links

--- app/[page]/page.tsx
+++ app/[page]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { articleUrl, getArticleById } from "../site";
